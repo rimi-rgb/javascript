fb3d51ec07d391a0bd6f34c0fef947b8bfda0328
Rimi Mondal <rimimondal325@gmail.com>
2026-07-25 16:55:07 +0530

    conversin of detatypes

---
+++ 01_basicscore/03_conversionOpretion.js
@@ -0,0 +1,26 @@
+let score = "Rimi"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" =>33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let IsloggedIn = "Rimi"
+
+let booleanIsloggedIn = Boolean(IsloggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+//"Rimi" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
