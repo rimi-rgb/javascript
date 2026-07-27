209a205568ba3d4ed182bfe745955364bb97a284
Rimi Mondal <rimimondal325@gmail.com>
2026-07-27 22:28:46 +0530

    arrow

---
+++ 03_basicscore/03_arrow.js
@@ -0,0 +1,46 @@
+const user = {
+    username: "Rimi",
+    price: 999,
+    
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+    }
+
+}
+
+//user.welcomeMessage()
+//user.username = "sam"
+//user.welcomeMessage()
+
+//console.log(this);
+
+//function chai(){
+//let username = "rimi"
+//console.log(this.username);
+//}
+
+const chai =  () => {
+    let username = "rimi"
+    console.log(this);
+    
+}
+
+//chai()
+
+//const addTwo = (num1, num2) => {
+ //   return num1 + num2
+//}
+
+//const addTwo = (num1, num2) => num1 + num2
+
+//const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => {{username: "rimi"}}
+
+console.log(addTwo(3,4))
+
+const myArray = [2, 5, 8, 3]
+
+//myArray.forEach()
