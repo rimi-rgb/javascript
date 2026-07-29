7df760c60f91bf18dd4d888800cf2f1eaf8a2c1d
Rimi Mondal <rimimondal325@gmail.com>
2026-07-29 08:35:22 +0530

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+//if
+
+const isUserLoggedIn = true
+const temperature = 40
+//if( temperature == 40 ){
+  //  console.log("executed");
+    //console.log("less then 50");
+    
+
+//} else {
+
+//console.log("temperature is gretar then 50");
+//}
+// <, >, <=, >=, ==, !=, ===, !==, 
+
+//const score = 200
+//if(score>100) {
+//    const power = "fly"
+//    console.log(`user power: ${power}`);
+//}
+ //console.log(`user power: ${power}`); 
+
+
+ const balance =1000
+
+ //if (balance > 500) console.log("test"),console.log("test 2");
+ 
+ const userLoggedIn = true 
+ const debitCard = true
+ const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+ if(userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy course");
+    
+ }
+
+ if (loggedInFromGoogle || loggedInFromEmail) {
+console.log("user logged in");
+
+ }
+  
+    
